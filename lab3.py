8fe04d14a600ecb0c9e4573b798377a4e3b7eb7e
eternazzz <alexkonder@mail.ru>
2022-10-07 14:27:48 +0300

    izm

--- lab3.py
+++ lab3.py
@@ -1,79 +1,51 @@
-
 import datetime as dt
-
-
 class Calculator:
-
-    def __init__(self, limit):
-        self.limit = limit
+    def __init__(self,limit):
+        self.limit=limit
+        self.today=dt.date.today()
+        self.week=self.today-dt.timedelta(7)
         self.records = []
-        self.today = dt.date.today()
-        self.week_ago = self.today - dt.timedelta(7)
-
-    def add_record(self, record):
+    def add_record(self,record):
         self.records.append(record)
-
     def get_today_stats(self):
-        day_stats = []
-        for record in self.records:
-            if record.date == self.today:
-                day_stats.append(record.amount)
-        return sum(day_stats)
-
+        today=dt.date.today()
+        today_stats=sum(Record.amount for record in records
+                        if record.date==today)
     def get_week_stats(self):
-        week_stats = []
+        week_stats=[]
         for record in self.records:
-            if self.week_ago <= record.date <= self.today:
+            if self.week<=record.date<=self.today:
                 week_stats.append(record.amount)
         return sum(week_stats)
-
-    def remained(self):
-        return(self.limit - self.get_today_stats())
-
-
-class CaloriesCalculator(Calculator):
-
-    def get_calories_remained(self):
-        calories_remained = self.remained()
-        if calories_remained > 0:
-            message = (f'Сегодня можно съесть что-нибудь ещё, но с общей'
-                       f'калорийностью не более {calories_remained} кКал')
-        else:
-            message = 'Хватит есть!'
-        return message
-
-
-class CashCalculator(Calculator):
-    USD_RATE = 56.3
-    EURO_RATE = 53.1
-    RUB_RATE = 1
-
-    def get_today_cash_remained(self, currency='rub'):
-        currencies = {'usd': ('USD', CashCalculator.USD_RATE),
-                      'eur': ('Euro', CashCalculator.EURO_RATE),
-                      'rub': ('руб', CashCalculator.RUB_RATE)}
-        cash_remained = self.remained()
-        if cash_remained == 0:
-            return 'Денег нет, держись'
-        if currency not in currencies:
-            return f'Валюта {currency} не поддерживается'
-        sign, rate = currencies[currency]
-        cash_remained = round(cash_remained / rate, 2)
-        if cash_remained > 0:
-            message = f'На сегодня осталось {cash_remained} {sign}'
-        else:
-            cash_remained = abs(cash_remained)
-            message = (f'Денег нет, держись: твой долг - {cash_remained} '
-                       f'{sign}')
-        return message
-
-
-class Record:
-
-    def __init__(self, amount, comment, date=None):
+    def get_today_remained(self):
+        remainder=self.limit-self.get_today_stats()
+        return remainder
+class Record(Calculator):
+    def __init__(self, amount, comment, date):
         self.amount = amount
         self.comment = comment
         if date is None:
-            self.date = dt.date.today()
+            self.date=dt.date.today()
         else:
-            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+            self.date = dt.datetime.strptime(date,'%d.%m.%Y').date()
+class CashCalculator(Calculator):
+    USD_ExchangeRate=60
+    EURO_ExchangeRate=57
+    RUB_ExchangeRate=1
+    def get_today_cash_remained(self,currency='rub'):
+        currencies={'usd':('USD',CashCalculator.USD_ExchangeRate),
+                    'eur':('Euro',CashCalculator.EURO_ExchangeRate),
+                    'rub':('Rub',CashCalculator.RUB_ExchangeRate)}
+        cash_remained=self.get_today_remained()
+        if cash_remained==0:
+            return 'Деньги закончились'
+        if cash_remained>0:
+            message=f'На сегодня осталось {cash_remained} {name}'
+        else:
+            cash_remained=abs(cash_remained)
+            message=(f'Деньги закончились, твой долг равен {cash_remained} {name}')
+        return  message
+if __name__=='main':
+    cash_calculator=CashCalculator(1000)
+    cash_calculator.add_record(Record(amount=145,comment="Кофе"))
+    print(cash_calculator.get_today_cash_remained("rub"))
